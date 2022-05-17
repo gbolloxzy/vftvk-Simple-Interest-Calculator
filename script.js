5ff568bb51f9bcaf44c1c285b255bc70544dc1b0
gbolloxzy <105552616+gbolloxzy@users.noreply.github.com>
2022-05-17 20:59:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function compute()
-{
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var amount = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-function updateRate()
-{
-    var rate = document.getElementById("rate").value;
-    document.getElementById("rate").innerText=rate;
-}
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
-function checkdata(){
-    var principal = document.getElementById("principal");
-    if(principal.value == ""){
-          alert("Please enter a positive number");
-          principal.focus();
-          return false;
-    }
-   alert("OK")
-    return true;
-}
+  function compute(){
+      var principal = parseFloat(document.getElementById("principal").value);
+      var rate = parseFloat(document.getElementById("rate").value);
+      var years = parseFloat(document.getElementById("years").value);
+      var result = principal * years * rate /100;
+      var years_modified = new Date().getFullYear() + parseInt(years);
+      document.getElementById("result").innerHTML = "If you deposit <mark>"
+     + principal + "</mark>, <br/> at an interest rate of <mark>"
+     + rate +"%.</mark><br/> You will receive an amount of <mark>" + result +
+     "</mark>,<br/>in the year <mark>" + years_modified + ".</mark><br/>";
+  }
+  function updateRate(){
+    var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+  }
+  function checkAmount(){
+    var principal = document.getElementById("principal").value;
+      if (principal <= 0) 
+      {
+    alert("Please enter a positive number");
+        document.getElementById("principal").focus();
+      }
+  }      
+  
